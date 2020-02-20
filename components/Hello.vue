5e196f7d4dfe68e85ded4ca30e52b3c7f1b00ab5
ymdma <m.yamada1985@gmail.com>
2020-02-20 18:58:20 +0900

    style_scoped

--- components/Hello.vue
+++ components/Hello.vue
@@ -4,19 +4,10 @@
   </h1>
 </template>
 
-<style>
+<style scoped>
 h1{
   margin: 0 70px 0 0;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
 
 </style>
