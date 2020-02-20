7b6535727bed8cc57752565b5828e1d5b3f7d092
ymdma <m.yamada1985@gmail.com>
2020-02-20 15:46:59 +0900

    first-commit_hello_nuxt

---
+++ components/Hello.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 class="title">
+    HelloNuxt!
+  </h1>
+</template>
+
+<style>
+h1{
+  margin: 0 70px 0 0;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+
+    <section class="title-view">
+      <logo />
+      <Hello />
+    </section>
+
+  <section class="message">
+    <h2>
+      {{ message }}
+    </h2>
+  </section>
+
+  </div>
+
+
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import Hello from '~/components/Hello.vue'
+
+export default {
+  data: function() {
+    return {
+      message: "My first Nuxt Project!"
+    }
+  },
+  components: {
+    Logo,
+    Hello
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  flex-direction: column;
+}
+.title-view{
+  width: 100%;
+  height:100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.message{
+  margin: 40px 0 0 0;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
